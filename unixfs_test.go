9826886c24014f2696728e5b6e8ffa2297b4dc34
Eric Chen <eric@tron.network>
2020-06-19 02:54:26 -0700

    Update BTFS deps

--- unixfs_test.go
+++ unixfs_test.go
@@ -10,19 +10,19 @@ import (
 	"path/filepath"
 	"testing"
 
+	chunker "github.com/TRON-US/go-btfs-chunker"
+	files "github.com/TRON-US/go-btfs-files"
+	unixfile "github.com/TRON-US/go-unixfs/file"
+	"github.com/TRON-US/go-unixfs/importer/balanced"
+	ihelper "github.com/TRON-US/go-unixfs/importer/helpers"
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-datastore"
 	dss "github.com/ipfs/go-datastore/sync"
 	bstore "github.com/ipfs/go-ipfs-blockstore"
-	chunker "github.com/ipfs/go-ipfs-chunker"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	files "github.com/ipfs/go-ipfs-files"
 	ipldformat "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
-	unixfile "github.com/ipfs/go-unixfs/file"
-	"github.com/ipfs/go-unixfs/importer/balanced"
-	ihelper "github.com/ipfs/go-unixfs/importer/helpers"
 	ipld "github.com/ipld/go-ipld-prime"
 	ipldfree "github.com/ipld/go-ipld-prime/impl/free"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
@@ -192,7 +192,7 @@ func TestUnixFSSelectorCopy(t *testing.T) {
 	Wish(t, err, ShouldEqual, nil)
 
 	// Setup a UnixFS file reader
-	n, err := unixfile.NewUnixfsFile(ctx, dagService2, otherNode)
+	n, err := unixfile.NewUnixfsFile(ctx, dagService2, otherNode, unixfile.UnixfsFileOptions{})
 	Wish(t, err, ShouldEqual, nil)
 	fn, ok := n.(files.File)
 	Wish(t, ok, ShouldEqual, true)
